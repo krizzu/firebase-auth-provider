f7b57317f77bc1cc123ba44acde1bed81b81b22e
Krzysztof Borowy <6444719+krizzu@users.noreply.github.com>
2024-09-01 21:12:47 +0200

    chore: Bump Kotlin + JDK compat (#9)

--- firebase-auth-provider/build.gradle.kts
+++ firebase-auth-provider/build.gradle.kts
@@ -1,7 +1,24 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
+
 plugins {
     kotlin("jvm")
     id("com.vanniktech.maven.publish")
 }
+
+kotlin {
+    compilerOptions {
+        apiVersion.set(KotlinVersion.KOTLIN_1_9)
+        languageVersion.set(KotlinVersion.KOTLIN_1_9)
+        jvmTarget.set(JvmTarget.JVM_17)
+    }
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
 
 dependencies {
     implementation(libs.firebase.admin) {
