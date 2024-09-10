5c7fa1fd705ff3d257fcc4f3d7fb393eb1dc823c
Michael Rittmeister <dr@schlau.bi>
2024-09-10 19:12:06 +0200

    Requested changes

--- firebase-auth-provider/src/main/kotlin/com/kborowy/authprovider/firebase/GoogleFutureUtil.kt
+++ firebase-auth-provider/src/main/kotlin/com/kborowy/authprovider/firebase/GoogleFutureUtil.kt
@@ -1,36 +1,54 @@
 package com.kborowy.authprovider.firebase
 
+
+package jetbrains.buildServer.clouds.google.connector
+
 import com.google.api.core.ApiFuture
-import com.google.common.util.concurrent.MoreExecutors
-import kotlinx.coroutines.CancellableContinuation
-import kotlinx.coroutines.suspendCancellableCoroutine
+import com.google.api.core.ApiFutureCallback
+import com.google.api.core.ApiFutures
+import com.google.common.util.concurrent.*
+import kotlinx.coroutines.*
 import java.util.concurrent.ExecutionException
+import kotlin.coroutines.Continuation
 import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
 
-internal suspend fun <T> ApiFuture<T>.await(): T {
-    if (isDone) {
-        try {
-            return get()
-        } catch (e: ExecutionException) {
-            throw e.cause ?: e // unwrap original cause from ExecutionException
-        }
+/**
+ * Awaits for completion of the future without blocking a thread.
+ *
+ * This suspending function is cancellable.
+ * If the [Job] of the current coroutine is completed while this suspending function is waiting, this function
+ * stops waiting for the future and immediately resumes with [CancellationException].
+ *
+ * Note, that `ListenableFuture` does not support removal of installed listeners, so on cancellation of this wait
+ * a few small objects will remain in the `ListenableFuture` list of listeners until the future completes. However, the
+ * care is taken to clear the reference to the waiting coroutine itself, so that its memory can be released even if
+ * the future never completes.
+ */
+ // https://github.com/JetBrains/teamcity-google-agent/blob/master/google-cloud-server/src/main/kotlin/jetbrains/buildServer/clouds/google/connector/ApiFuture.kt#L27-L52
+suspend fun <T> ApiFuture<T>.await(): T {
+    try {
+        if (isDone) return get() as T
+    } catch (e: ExecutionException) {
+        throw e.cause ?: e // unwrap original cause from ExecutionException
     }
 
-    // slow path -- suspend
     return suspendCancellableCoroutine { cont: CancellableContinuation<T> ->
-        this.addListener(
-            {
-                try {
-                    cont.resume(get())
-                } catch (e: ExecutionException) {
-                    cont.resumeWithException(e.cause ?: e) // unwrap original cause from ExecutionException
-                }
-            },
-            MoreExecutors.directExecutor()
-        )
+        val callback = ContinuationCallback(cont)
+        ApiFutures.addCallback(this, callback, MoreExecutors.directExecutor())
         cont.invokeOnCancellation {
             cancel(false)
+            callback.cont = null // clear the reference to continuation from the future's callback
         }
     }
+}
+
+private class ContinuationCallback<T>(
+        @Volatile @JvmField var cont: Continuation<T>?
+) : ApiFutureCallback<T> {
+    override fun onSuccess(result: T?) {
+        @Suppress("UNCHECKED_CAST")
+        cont?.resume(result as T)
+    }
+    override fun onFailure(t: Throwable) { cont?.resumeWithException(t) }
 }
