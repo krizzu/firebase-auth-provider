58a2abce46b21e7d48db6f1657f8b6c6d742622a
Krzysztof Borowy <6444719+krizzu@users.noreply.github.com>
2025-07-22 22:11:36 +0200

    chore: spotless fix

--- firebase-auth-provider/src/main/kotlin/com/kborowy/authprovider/firebase/FirebaseAuthConfig.kt
+++ firebase-auth-provider/src/main/kotlin/com/kborowy/authprovider/firebase/FirebaseAuthConfig.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2025 Krzysztof Borowy
+ * Copyright 2023 Krzysztof Borowy
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
